8c9edb06f23bd35005501fbc03a013e5d638e5d7
Francisco <franciscovergaracr@gmail.com>
2021-05-29 17:01:53 -0300

    Creacion de componente

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Todolist from './Components/Todolist/todolist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to Pomodoro</p>
+        <Todolist/>
       </header>
     </div>
   );
---
+++ pomodoro/src/Components/Todolist/todolist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './todolist.css';
+
+class Todolist extends React.Component {
+    render(){
+        return (
+            <div >
+                <p>Objetivos:</p>
+                <ol>
+                    <li>Tarea 1</li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default Todolist;
