9991e0234f7f356987f00657b5453495b8d8a984
Francisco <franciscovergaracr@gmail.com>
2021-05-30 14:14:39 -0300

    Primera version

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Todolist from './Components/Todolist/todolist';
+import CountDownTimer from './Components/Timer/timer';
 
 function App() {
+
+  const hoursMinSecs = {hours:0, minutes: 0, seconds: 20}
+
   return (
     <div className="App">
       <header className="App-header">
+        <CountDownTimer hoursMinSecs={hoursMinSecs}/>
         <p>Welcome to Pomodoro</p>
         <Todolist/>
       </header>
--- pomodoro/src/Components/Todolist/todolist.js
+++ pomodoro/src/Components/Todolist/todolist.js
@@ -2,13 +2,52 @@ import React from 'react';
 import './todolist.css';
 
 class Todolist extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            etiqueta: '',
+            trabajoRealizado: [],
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        let temporal = this.state.trabajoRealizado;
+        console.log(temporal);
+        temporal.push(this.state.etiqueta);
+        console.log(temporal);
+        this.setState(
+        {
+            trabajoRealizado: temporal,
+        }
+        );
+
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        })
+    }
     render(){
         return (
             <div >
-                <p>Objetivos:</p>
-                <ol>
-                    <li>Tarea 1</li>
-                </ol>
+                <form>
+                    <label>Etiqueta tu pomodoro </label>
+                        <input type="text" name="etiqueta" onChange={this.handleInputChange}/>
+                        <input type="submit" value="Comenzar" onClick={this.handleSubmit.bind(this)} />
+                </form>
+                <ul>
+                    {this.state.trabajoRealizado.map(string => 
+                    <li>{string}</li>
+                    )}
+                </ul>
             </div>
         )
     }
